d8667d840a9feef85504041e0d008f6495c78b0e
GreenLu <green.ee02g@g2.nctu.edu.tw>
2020-05-30 22:46:41 +0800

    clear button

--- WebApp/calculator/urls.py
+++ WebApp/calculator/urls.py
@@ -2,7 +2,7 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('', views.calculator),
+    path('', views.calculator, name='home'),
     path('calculator/<str:newResult>/<str:lastResult>/<str:lastOperator>/<int:reset>/', views.calculator),
     path('calculator/digit/<str:digit>/<str:previous>/<str:lastResult>/<str:lastOperator>/<int:reset>/', views.digitInput, name= 'digitInput'),
     path('calculator/operator/<str:operator>/<str:newResult>/<str:lastResult>/<str:lastOperator>/<int:reset>/', views.operatorInput, name= 'operatorInput'),
