c62c71f6ae476f3a7b440180306c1770f715d881
GreenLu <green.ee02g@g2.nctu.edu.tw>
2020-05-31 14:25:59 +0800

    warning alert

--- WebApp/calculator/views.py
+++ WebApp/calculator/views.py
@@ -8,18 +8,19 @@ operate = {
     }
 DISPLAY_MAX_LENGTH = 14
 
-def calculator(request, newResult='0', lastResult='0', lastOperator='equals', reset=1):
+def calculator(request, newResult='0', lastResult='0', lastOperator='equals', reset=1, alert=1):
     content = {
         'newResult' : newResult,
         'lastResult' : lastResult,
         'lastOperator' : lastOperator,
         'reset': reset,
+        'alert':alert,
     }
     return  render(request, 'calculator/calculator.html', content)
 
 def digitInput(request, digit, previous, lastResult, lastOperator, reset):
     global DISPLAY_MAX_LENGTH
-    if reset:
+    if reset or previous=='0':
         newResult = str(digit)
     elif len(previous) >= DISPLAY_MAX_LENGTH:
         newResult = previous
@@ -32,11 +33,14 @@ def operatorInput(request, operator, newResult, lastResult, lastOperator, reset)
     global operate
     if reset:
         return redirect(calculator,newResult=newResult,lastResult=lastResult, lastOperator=operator, reset=1)
-    elif lastOperator === 'equals':
+    elif lastOperator == 'equals':
         return redirect(calculator,newResult=newResult,lastResult=newResult, lastOperator=operator, reset=1)
     else:
-        result = operate[lastOperator](int(lastResult),int(newResult))
-    return redirect(calculator,newResult=result,lastResult=result, lastOperator=operator, reset=1)
+        try:
+            result = operate[lastOperator](int(lastResult),int(newResult))
+            return redirect(calculator,newResult=result,lastResult=result, lastOperator=operator, reset=1)
+        except :
+            return redirect(calculator)
 
 
 
