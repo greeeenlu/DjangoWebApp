0f552811789ea4c74d5ae4dd92c1618825cdf56b
GreenLu <green.ee02g@g2.nctu.edu.tw>
2020-06-19 02:29:08 +0800

    processing overflow of display

--- WebApp/calculator/views.py
+++ WebApp/calculator/views.py
@@ -4,11 +4,25 @@ from django.shortcuts import render
 # Create your views here.
 def calculator(request):
     content = {
-        "displayResult": 0
+        'displayResult': 0,
+        'lastOperator': '=',
+        'previousValue': 0,
+        'newValue': 0,
+        'isOperatorClicked': False
     }
     if request.method == 'POST':
         form = request.POST
         print(form)
+        if 'newValue' in form:
+            content['newValue'] = int(form['newValue'])
         if 'digitalInput' in form:
-            content['displayResult'] = form['digitalInput']
+            newValue = content['newValue'] * 10 + int(form['digitalInput'])
+            print(newValue)
+            if newValue > 9999999999999:
+                content['displayResult'] = content['newValue']
+                return render(request, 'calculator/calculator.html', content)
+            else:
+                content['displayResult'] = newValue
+                content['newValue'] = newValue
+            print(content)
     return render(request, 'calculator/calculator.html', content)
