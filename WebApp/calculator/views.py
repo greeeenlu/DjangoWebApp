b19b69adb76b56ba107bf482721e286303222ed8
GreenLu <green.ee02g@g2.nctu.edu.tw>
2020-06-18 21:50:13 +0800

    form processing digit input

--- WebApp/calculator/views.py
+++ WebApp/calculator/views.py
@@ -4,6 +4,11 @@ from django.shortcuts import render
 # Create your views here.
 def calculator(request):
     content = {
-        "displayResult": 4
+        "displayResult": 0
     }
+    if request.method == 'POST':
+        form = request.POST
+        print(form)
+        if 'digitalInput' in form:
+            content['displayResult'] = form['digitalInput']
     return render(request, 'calculator/calculator.html', content)
