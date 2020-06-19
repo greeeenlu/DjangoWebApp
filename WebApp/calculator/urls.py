bcfee289270a9c4e1813a0b89d3a88a8cb088333
GreenLu <green.ee02g@g2.nctu.edu.tw>
2020-06-19 14:27:50 +0800

    add home/reset button

--- WebApp/calculator/urls.py
+++ WebApp/calculator/urls.py
@@ -2,6 +2,6 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('', views.calculator),
+    path('', views.calculator, name='index'),
     path('calculator/', views.calculator)
 ]
